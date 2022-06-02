7ba9de9a1c33a31c1a03905c3119b580c43bb962
Nikhail <67645752+nikhailmann@users.noreply.github.com>
2022-06-01 20:40:52 -0400

    Add files via upload

---
+++ cses/introductory problems/trailingzeros.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using vi = vector<int>;
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using pi = pair<int,int>;
+#define F first
+#define S second
+#define mp make_pair
+
+using ll = long long;
+using db = long double;
+using str = string;
+#define lb lower_bound  // First element NOT LESS than val
+#define ub upper_bound  // First element GREATER than val
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+
+const int MOD = 1e9+7;
+const int INF = 1e9;
+
+
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll n;
+    cin >> n;
+    ll num = 0;
+    for (int i = 5; n/i >= 1; i*=5) {
+        num += n/i;
+    }
+    cout << num;
+
+
+
+    return 0;
+}
