1a5974e1cd7126acbf0c28b4b7884d48f7e93ae3
Nikhail <67645752+nikhailmann@users.noreply.github.com>
2022-07-20 18:00:13 -0400

    Add files via upload

---
+++ cses/dynamic programming/gridPathsDP.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using vi = vector<int>;
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using pi = pair<int,int>;
+#define F first
+#define S second
+#define mp make_pair
+
+using ll = long long;
+using db = long double;
+using str = string;
+#define lb lower_bound  // First element NOT LESS than val
+#define ub upper_bound  // First element GREATER than val
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+const int dr[4] = {1, 0, -1, 0};
+const int dc[4] = {0, 1, 0, -1};
+
+const int MOD = 1e9+7;
+const int INF = 1e9;
+
+
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+ll dp[1000][1000];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    string board[n];
+    for (int i = 0; i < n; i++) {
+        cin >> board[i];
+    }
+    dp[0][0] = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <n; j++) {
+            if (board[i][j] == '*') {
+                dp[i][j] = 0;
+            } else {
+                if (i>0) {
+                    dp[i][j] += dp[i-1][j];
+                }
+                if (j>0) {
+                    dp[i][j] += dp[i][j-1];
+                }
+            }
+            dp[i][j] %= MOD;
+        }
+    }
+    cout << dp[n-1][n-1];
+
+
+
+
+    return 0;
+}
