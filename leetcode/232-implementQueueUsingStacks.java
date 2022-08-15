3beb266fd5c2eaf102fd502718c5d7210a01144c
Nikhail <67645752+nikhailmann@users.noreply.github.com>
2022-08-15 18:36:05 -0400

    Update 232-implementQueueUsingStacks.java

--- leetcode/232-implementQueueUsingStacks.java
+++ leetcode/232-implementQueueUsingStacks.java
@@ -1,36 +1,36 @@
 class MyQueue {
     
-    Stack<Integer> stack1 = new Stack();
-    Stack<Integer> stack2 = new Stack();
+    private Stack<Integer> stack1 = new Stack();
+    private Stack<Integer> stack2 = new Stack();
     public MyQueue() {
         
     }
     
     public void push(int x) {
-        this.stack1.push(x);
+        stack1.push(x);
     }
     
     public int pop() { //all calls are valid so don't worry
-        if (this.stack2.isEmpty()) {
-            while (!this.stack1.isEmpty()) {
-                this.stack2.push(stack1.pop());
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
             }
         }
-        return this.stack2.pop();
+        return stack2.pop();
         
     }
     
     public int peek() {
-        if (this.stack2.isEmpty()) {
-            while (!this.stack1.isEmpty()) {
-                this.stack2.push(stack1.pop());
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
             }
         }
-        return this.stack2.peek();
+        return stack2.peek();
     }
     
     public boolean empty() {
-        if (this.stack1.isEmpty() && this.stack2.isEmpty()) {
+        if (stack1.isEmpty() && stack2.isEmpty()) {
             return true;
         }
         return false;
