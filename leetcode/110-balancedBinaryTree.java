25401529afa8030be67bbe080046b77d58fe64f6
Nikhail <67645752+nikhailmann@users.noreply.github.com>
2022-08-16 21:31:55 -0400

    Update 110-balancedBinaryTree.java

--- leetcode/110-balancedBinaryTree.java
+++ leetcode/110-balancedBinaryTree.java
@@ -13,6 +13,27 @@
  *     }
  * }
  */
+class Solution { //Bottom up solution!
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return isBalancedAux(root) != -2;
+    }
+    
+    public Integer isBalancedAux(TreeNode root) {
+        if (root == null) {
+            return -1;
+        }
+        int l = isBalancedAux(root.left);
+        int r = isBalancedAux(root.right);
+        if (l == -2 || r == -2 || Math.abs(l-r) > 1) {
+            return -2;
+        }
+        return 1 + Math.max(l, r);
+        
+    }
+}
+
+/** //top down solution is slower
 class Solution {
     public boolean isBalanced(TreeNode root) {
         if (root == null) return true;
@@ -23,9 +44,10 @@ class Solution {
     
     public Integer isBalancedAux(TreeNode root) {
         if (root == null) {
-            return 0;
+            return -1; //-1 not 0
         } else {
             return 1 + Math.max(isBalancedAux(root.left), isBalancedAux(root.right));
         }
     }
 }
+**/
